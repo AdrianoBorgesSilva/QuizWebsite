3e79d1dc5ca365b8775ad759b120af4b6ac7e563
Adriano Borges Silva <120127965+AdrianoBorgesSilva@users.noreply.github.com>
2023-04-12 18:09:23 -0300

    Deleted files

--- JS_page/js.js
+++
@@ -1,9 +0,0 @@
-const quizz_btn = document.getElementById('quizz_btn')
-const quiz_questions = document.getElementById('quiz_questions')
-
-
-quizz_btn.addEventListener('click', ()=>{
-    quiz_questions.classList.toggle('show')
-    
-})
-
