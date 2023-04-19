3757bfd07c7817fd327660daa34ce324e31070e2
Adriano Borges Silva <120127965+AdrianoBorgesSilva@users.noreply.github.com>
2023-04-19 16:49:06 -0300

    Update

--- main_page/script.js
+++ main_page/script.js
@@ -2,5 +2,5 @@ const sidebar_arrow = document.getElementById('sidebar_arrow')
 const sidebar = document.getElementById('sidebar')
 
 sidebar_arrow.addEventListener('click', ()=>{
-    sidebar.classList.toggle('sidebar_show')
+    sidebar.classList.add('sidebar_show')
 })
